ca5c588766c1739a111a573fab836f9ff849cd32
Nikita Raven <esh2015@list.ru>
2022-12-02 19:12:51 +0400

    прописал функцию formatted_file

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ from typing import Union
 import autopep8
 
 
+def formatted_file(input_file: str) -> pd.DataFrame:
+    df = pd.read_csv(input_file)
+    df["Day"] = pd.to_datetime(df.Day, format="%Y-%m-%d")
+    df["Day"] = df["Day"].dt.date
+    return df
+
+
